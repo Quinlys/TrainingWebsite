13a51d926b12606dd13465f85b6fead01a173e75
Igor <yakymiv.ig@gmail.com>
2020-06-05 04:33:30 +0300

    Upload adaptive menu

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById('menu').onclick = function () {
+    let x = document.getElementById('myTopnav');
+    if (x.className === 'topnav') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'topnav';
+    }
+};
